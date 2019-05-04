6fdd1e2d1345d36374714880ad5682469c2352c2
shivam mishra <s.kmishra703@gmail.com>
2019-05-04 17:42:01 +0530

    added dir creation in home

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@
 CON_DEV=$(jmtpfs -l | grep Device | wc -l)
 
 
-
+#########################LISTING###################################
 echo -e \\n
 for i in $(seq 1 $CON_DEV)
 do
@@ -13,15 +13,46 @@ do
 done
 echo -e "\\nTotal Devices Connected With MTP : $CON_DEV\\n"
 
+#########################MOUNTING###################################
+
 read -p "Select Device : " in
 
-echo -e Mounting $(jmtpfs -l | cut -d "," -f6 | sed '1,2d' | sed -n "${in}p")
 
+DEV_NAME=$(jmtpfs -l | cut -d "," -f6 | sed '1,2d' | sed -n "${in}p" | tr -d '[:space:]')
+if [[ -d ~/$DEV_NAME ]]
+then
+	echo Directory $DEV_NAME Exist! Remove it?
+	read choice
+	case $choice in
+		y|Y|yes|YES)
+			rm -rf ~/$DEV_NAME
+			;;
+		n|N|no|NO)
+			echo OKAY BYYY
+			exit 0
+			;;
+		*)
+			echo Invalid Choice
+		        ;;
+	esac	
+fi
+
+mkdir ~/$DEV_NAME
+if [[ $? -eq 0 ]] 
+then
+	echo ~/$DEV_NAME Created
+else
+	exit 0
+fi
+
+echo Mounting $DEV_NAME
 BUS=$(jmtpfs -l | cut -d "," -f1 | sed '1,2d' | sed -n "${in}p")
 DEV=$(jmtpfs -l | cut -d "," -f2 | sed '1,2d' | sed -n "${in}p" | tr -d '[:space:]')
-if $(jmtpfs -device=${BUS},${DEV} /mnt/OP5T)
+if $(jmtpfs -device=${BUS},${DEV} ~/$DEV_NAME)
 then
 	echo Mounting Successfull
 else
 	echo Failed
 fi
+
+
